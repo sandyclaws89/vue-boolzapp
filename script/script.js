d214f502ea6f409a3935e8166a814dd923262c0d
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-16 10:46:17 +0100

    creato array di oggeti per la lista chat

---
+++ script/script.js
@@ -0,0 +1,56 @@
+const app = new Vue({
+    el:'#root',
+    data :{
+        chatList:[
+            {
+                pic:"../img/avatar_1.jpg",
+                name:"Michele",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_2.jpg",
+                name:"Fabio",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_3.jpg",
+                name:"Samuele",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_4.jpg",
+                name:"Alessandro B.",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_5.jpg",
+                name:"Claudia",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_6.jpg",
+                name:"Federico",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_7.jpg",
+                name:"Michele",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+            {
+                pic:"../img/avatar_8.jpg",
+                name:"Davide",
+                lastMessage: "Ultimo messaggio inviato",
+                time:"12.00",
+            },
+        ]
+    }
+})
+console.log(chatList.pic)

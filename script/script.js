b050c6f26052c90da6cf6fa909ac7b0633acdb62
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-16 15:24:31 +0100

    sistemata struttura dati

--- script/script.js
+++ script/script.js
@@ -8,7 +8,23 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['1','2'],
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_2.jpg",
@@ -16,7 +32,23 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['3','4'],
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_3.jpg",
@@ -24,7 +56,23 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['5','6'],
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_4.jpg",
@@ -32,7 +80,18 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['7','8']
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_5.jpg",
@@ -40,7 +99,23 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['9','10']
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao Claudia, hai novità?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Non ancora',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'Nessuna nuova, buona nuova',
+                        status: 'sent'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_6.jpg",
@@ -48,7 +123,18 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['11','12']
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                        status: 'received'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_7.jpg",
@@ -56,7 +142,18 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['13','14']
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ricordati di chiamare la nonna',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Va bene, stasera la sento',
+                        status: 'received'
+                    }
+                ],
             },
             {
                 pic:"../img/avatar_8.jpg",
@@ -64,7 +161,23 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 time:"12.00",
                 text:'',
-                chat:['15','16']
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'OK!!',
+                        status: 'received'
+                    }
+                ],
             },
         ],
         myMessage:[{
